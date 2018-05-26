9ab5e6bf749d300d5ae9c3e326c98604daaf6b18
Leena Nofal <leenanofal@gmail.com>
2018-05-26 01:52:21 +0000

    added file to load images and now able to print image_ids

--- train.py
+++ train.py
@@ -1,20 +1,23 @@
 import numpy as np 
 import json
-
-
+import imageloader
+from imutils import paths
 
 def main():
     jsonFile = open('/home/Leena/dataset/photos/photos.json')
     infos = []
     #loop through the lines in the json file and append each one into the infos array
+    imagePaths = list(paths.list_images('/home/Leena/dataset/photos/photos/'))
     for line in jsonFile:
         photo_info = json.loads(line) #gets each line in the json file
         infos.append(photo_info)
-
+    il = imageloader.SimpleDatasetLoader(preprocessors=[]) #building the image loader
+    (images, img_ids) = il.load(imagePaths, max_images=1000, verbose=500)
+    
 
     for i in range(10):
         print(infos[i]['label']) #json format allows you to do this: gets the label value from each line
-
+        print(img_ids[i])
 
 if __name__ ==  "__main__":
     main()
